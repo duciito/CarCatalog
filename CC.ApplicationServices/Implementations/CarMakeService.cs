bb474963b2fe1463b72235c81ec54481bd65b5ab
Daniel Ivanov <ducciito@hotmail.com>
2019-05-31 22:57:53 +0300

    add CarMake and CarTyper services

--- CC.ApplicationServices/Implementations/CarMakeService.cs
+++ CC.ApplicationServices/Implementations/CarMakeService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CC.ApplicationServices.DTOs;
+using CC.Data.Entities;
+using CC.Repositories.Implementations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,97 @@ using System.Threading.Tasks;
 
 namespace CC.ApplicationServices.Implementations
 {
-    class CarMakeService
+    public class CarMakeService
     {
+        public List<CarMakeDto> Get()
+        {
+            List<CarMakeDto> carMakeDtos = new List<CarMakeDto>();
+
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                foreach (var item in unitOfWork.CarMakeRepository.Get())
+                {
+                    carMakeDtos.Add(new CarMakeDto
+                    {
+                        Id = item.Id,
+                        Name = item.Name,
+                        Country = item.Country,
+                        Description = item.Description
+                    });
+                }
+            }
+            return carMakeDtos;
+        }
+
+        public CarMakeDto GetById(int id)
+        {
+            CarMakeDto carMakeDto;
+
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                CarMake carMakeEntity = unitOfWork.CarMakeRepository.GetById(id);
+
+                carMakeDto = new CarMakeDto
+                {
+                    Id = carMakeEntity.Id,
+                    Name = carMakeEntity.Name,
+                    Country = carMakeEntity.Country,
+                    Description = carMakeEntity.Description
+                };
+            }
+            return carMakeDto;
+        }
+
+        public bool Save(CarMakeDto carMakeDto)
+        {
+            CarMake carMakeEntity = new CarMake
+            {
+                Id = carMakeDto.Id,
+                Name = carMakeDto.Name,
+                Country = carMakeDto.Country,
+                Description = carMakeDto.Description
+            };
+
+            try
+            {
+                using (UnitOfWork unitOfWork = new UnitOfWork())
+                {
+                    if (carMakeDto.Id == 0)
+                    {
+                        unitOfWork.CarMakeRepository.Insert(carMakeEntity);
+                    }
+                    else
+                    {
+                        unitOfWork.CarMakeRepository.Update(carMakeEntity);
+                    }
+                    unitOfWork.Save();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            if (id == 0) return false;
+
+            try
+            {
+                using (UnitOfWork unitOfWork = new UnitOfWork())
+                {
+                    CarMake carMakeEntity = unitOfWork.CarMakeRepository.GetById(id);
+                    unitOfWork.CarMakeRepository.Delete(carMakeEntity);
+                    unitOfWork.Save();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
--- CC.ApplicationServices/Implementations/CarTypeService.cs
+++ CC.ApplicationServices/Implementations/CarTypeService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CC.ApplicationServices.DTOs;
+using CC.Data.Entities;
+using CC.Repositories.Implementations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,94 @@ using System.Threading.Tasks;
 
 namespace CC.ApplicationServices.Implementations
 {
-    class CarTypeService
+    public class CarTypeService
     {
+        public List<CarTypeDto> Get()
+        {
+            List<CarTypeDto> carTypeDtos = new List<CarTypeDto>();
+
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                foreach (var item in unitOfWork.CarTypeRepository.Get())
+                {
+                    carTypeDtos.Add(new CarTypeDto
+                    {
+                        Id = item.Id,
+                        Name = item.Name,
+                        Description = item.Description
+                    });
+                }
+            }
+            return carTypeDtos;
+        }
+
+        public CarTypeDto GetById(int id)
+        {
+            CarTypeDto carTypeDto;
+
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                CarType carTypeEntity = unitOfWork.CarTypeRepository.GetById(id);
+
+                carTypeDto = new CarTypeDto
+                {
+                    Id = carTypeEntity.Id,
+                    Name = carTypeEntity.Name,
+                    Description = carTypeEntity.Description
+                };
+            }
+            return carTypeDto;
+        }
+
+        public bool Save(CarTypeDto carTypeDto)
+        {
+            CarType carTypeEntity = new CarType
+            {
+                Id = carTypeDto.Id,
+                Name = carTypeDto.Name,
+                Description = carTypeDto.Description
+            };
+
+            try
+            {
+                using (UnitOfWork unitOfWork = new UnitOfWork())
+                {
+                    if (carTypeDto.Id == 0)
+                    {
+                        unitOfWork.CarTypeRepository.Insert(carTypeEntity);
+                    }
+                    else
+                    {
+                        unitOfWork.CarTypeRepository.Update(carTypeEntity);
+                    }
+                    unitOfWork.Save();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            if (id == 0) return false;
+
+            try
+            {
+                using (UnitOfWork unitOfWork = new UnitOfWork())
+                {
+                    CarType carTypeEntity = unitOfWork.CarTypeRepository.GetById(id);
+                    unitOfWork.CarTypeRepository.Delete(carTypeEntity);
+                    unitOfWork.Save();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
