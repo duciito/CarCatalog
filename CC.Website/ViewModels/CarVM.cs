aa0cc44c6b649403f0d8d2599f64053791e80ea2
Daniel Ivanov <ducciito@hotmail.com>
2019-06-13 00:18:22 +0300

    add styling of table, inputs and description lists

--- CC.Website/ViewModels/CarVM.cs
+++ CC.Website/ViewModels/CarVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -12,15 +13,21 @@ namespace CC.Website.ViewModels
         [Required(ErrorMessage = "Field is required!")]
         [StringLength(200, MinimumLength = 1, ErrorMessage = "Field must be between 1 and 200 characters!")]
         public string Model { get; set; }
+        [DisplayName("Year")]
         [Required(ErrorMessage = "Field is required!")]
         public string ReleaseYear { get; set; }
 
+        [DisplayName("Horse power")]
         public int HorsePower { get; set; }
 
+        [DisplayName("Make")]
         public int MakeId { get; set; }
+        [DisplayName("Type")]
         public int TypeId { get; set; }
 
+        [DisplayName("Make")]
         public string MakeName { get; set; }
+        [DisplayName("Type")]
         public string TypeName { get; set; }
         public SelectList CarTypes { get; set; }
         public SelectList CarMakes { get; set; }
